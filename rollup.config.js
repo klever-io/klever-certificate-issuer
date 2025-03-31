161d21b71b3dace0fb1c52f2f2e98931becd9dd5
Beroni <beronimagalhaes@gmail.com>
2025-03-31 17:01:28 -0300

    chore: initial config

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import { terser } from "rollup-plugin-terser";
+
+import { readFileSync } from "fs";
+
+const packageJson = JSON.parse(
+  readFileSync(new URL("./package.json", import.meta.url), "utf-8")
+);
+
+const config = [
+  {
+    input: "lib/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json", downlevelIteration: true }),
+      terser(),
+    ],
+  }
+];
+
+export default config;
