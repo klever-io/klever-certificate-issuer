aa551b2903702c1942e10b9954e05602f3ba9dfc
Beroni <beronimagalhaes@gmail.com>
2025-04-14 14:35:45 -0300

    chore: export IProvider

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,5 @@
 import { Certificate, EventsResponse } from './certificate'
 import { flattenJSON, unflattenJSON } from './utils'
+import { IProvider } from '@klever/sdk-node'
 
-export { Certificate, EventsResponse, flattenJSON, unflattenJSON }
+export { Certificate, EventsResponse, IProvider, flattenJSON, unflattenJSON }
